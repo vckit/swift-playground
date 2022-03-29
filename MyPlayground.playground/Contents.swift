4639ed48fb3c966316f2db8f10bedbdbc2c3ca61
Abudlkhakim Magomedov <thevckit@gmail.com>
2022-03-29 09:24:23 +0300

    Замыкания

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// потоки
+let numberOfLegs = ["spider": 8, "ant": 3, "cat": 10]
+for(animalName, legCount) in numberOfLegs {
+    print("\(animalName)s have \(legCount) legs")
+}
+
+// замыкания
+let names = ["Zok", "Alex", "Alexandr", "Mark", "Maks"]
+func backward(_ s1: String, _ s2: String) -> Bool {
+    return s1 < s2
+}
+var reservedName = names.sorted(by: backward)
+print(reservedName)
+
+var reservedNameSecond = names.sorted(by: {(s1: String, s2: String) -> Bool in
+    return s1 < s2
+})
+print(reservedNameSecond)
